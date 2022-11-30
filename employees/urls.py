5b4702333b1de952f3a452896c396ec356f1d6e2
Hatesnake69 <vladimir@itsy.dev>
2022-12-01 01:28:49 +0300

    [*] change uuid to int

--- employees/urls.py
+++ employees/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<uuid:subdivision_id>/", views.list_all_subdivision_elements),
+    path("<int:subdivision_id>/", views.list_all_subdivision_elements),
     path("", views.index),
 ]
