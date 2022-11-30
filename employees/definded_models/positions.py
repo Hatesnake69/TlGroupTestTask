8aa6d0b76d3cf047047e774590ad0012c08ecf08
Hatesnake69 <vladimir@itsy.dev>
2022-11-30 15:14:19 +0300

    [+] add CustomModelManager

--- employees/definded_models/positions.py
+++ employees/definded_models/positions.py
@@ -1,12 +1,14 @@
 import uuid
-
 from django.db import models
+
+from employees.definded_models.manager import CustomModelManager
 
 
 class PositionModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=512)
+    name = models.CharField(max_length=512, unique=True)
     description = models.TextField()
+    objects = CustomModelManager()
 
     class Meta:
         managed = True
