d0a6c5cb72f7d9355f466a080ceecf2341ff283e
Hatesnake69 <vladimir@itsy.dev>
2022-11-30 20:51:35 +0300

    [+] add views

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from employees.models import EmployeeModel, SubdivisionModel
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def index(request):
+    root = SubdivisionModel.objects.get(parent_id=None)
+    viewed_subdivisions = SubdivisionModel.objects.filter(parent_id=root.id)
+    viewed_employees = EmployeeModel.objects.filter(subdivision_id=root.id)
+    return render(
+        request,
+        "employees/index.html",
+        {
+            "root": root,
+            "viewed_subdivisions": viewed_subdivisions,
+            "viewed_employees": viewed_employees,
+        },
+    )
+
+
+def list_all_subdivision_elements(request, subdivision_id):
+
+    root = SubdivisionModel.objects.get(pk=subdivision_id)
+    viewed_subdivisions = SubdivisionModel.objects.filter(parent_id=root.id)
+    viewed_employees = EmployeeModel.objects.filter(subdivision_id=subdivision_id)
+    return render(
+        request,
+        "employees/index.html",
+        {
+            "root": root,
+            "viewed_subdivisions": viewed_subdivisions,
+            "viewed_employees": viewed_employees,
+        },
+    )
+
+
+def redirect_view(request):
+    response = redirect('/subdivision/')
+    return response
