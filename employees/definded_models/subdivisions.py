71ab23564ba7f0e97f73037265b4587cac819531
Hatesnake69 <vladimir@itsy.dev>
2022-11-30 15:14:52 +0300

    [+] add parent

--- employees/definded_models/subdivisions.py
+++ employees/definded_models/subdivisions.py
@@ -1,12 +1,18 @@
 import uuid
-
 from django.db import models
+
+from employees.definded_models.manager import CustomModelManager
 
 
 class SubdivisionModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=512)
     description = models.TextField()
+    parent = models.ForeignKey(
+        to="employees.SubdivisionModel", name="parent", on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+    objects = CustomModelManager()
 
     class Meta:
         managed = True
